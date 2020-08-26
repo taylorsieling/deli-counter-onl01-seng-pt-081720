f2358369635d1f1fbc8422bccf40136f122cae23
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 22:21:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ katz_deli = []
 
 def line(katz_deli)
  while katz_deli.empty? = true
- puts 
+ puts "The line is currently empty."
+ else
  katz_deli.each_with_index do |name, i|
    puts "The line is currently: #{index +1}. #{name}"
  end
- else 
 end
