5a0ca00a82c173e420fe7c3f61062d190e65b9ee
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 22:13:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,3 @@
 katz_deli = []
 
 def line(katz_deli) 
-  
-  
-  "The line is currently: 1. Logan 2. Avi 3. Spencer"
-end
-
-def take_a_number(katz_deli, "name")
-  katz_deli.each_with_index do |x, i|
-    
-  puts "Welcome, #{name}. You are number #{place} in line."
-end
-
-def now_serving(katz_deli)
-  puts 
-end
