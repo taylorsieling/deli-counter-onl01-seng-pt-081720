702b328691d870d4b52a8c515b0a48280003e007
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 22:04:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, "name")
-  katz_deli.each_with_index
+  katz_deli.each_with_index do |
   puts "Welcome, #{name}. You are number #{place} in line."
 end
 
