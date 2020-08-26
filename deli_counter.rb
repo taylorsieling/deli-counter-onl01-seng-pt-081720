e7e4f168f7f4336f907a48c3458cdfe1fc58db7f
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 22:36:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 katz_deli = []
 
 def line(x)
- line_array =[]
- if x.length == 0
- puts "The line is currently empty."
- else
- x.each_with_index(1) do |name, index|
-   line.push("#{index +1}. #{name}")
- end
- puts "The line is currently: #{line_array.join(" ")}"
-end
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
 end
 
 def take_a_number(katz_deli, name)
