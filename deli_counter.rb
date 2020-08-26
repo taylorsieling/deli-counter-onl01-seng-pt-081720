00ac000dab360b3996fcaece33d70e46ec768609
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 22:25:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(katz_deli)
  while katz_deli.empty? == true
  puts "The line is currently empty."
  else
- katz_deli.each_with_index do |name, i|
+ katz_deli.each_with_index do |name, index|
    puts "The line is currently: #{index +1}. #{name}"
  end
 end
 
-katz_deli(name, i)
-other_deli(name, i)
+katz_deli(name, index)
+other_deli(name, index)
