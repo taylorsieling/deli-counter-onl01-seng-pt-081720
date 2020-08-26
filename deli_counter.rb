5794b16261c675a851053679921fc0350022400d
Taylor Sieling <taylorasieling@gmail.com>
2020-08-26 22:30:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,42 @@
 katz_deli = []
-other_deli = ["Logan", "Avi", "Spencer"]
 
-def line(katz_deli)
- while katz_deli.empty? == true
+def line(x)
+ line_array =[]
+ if x.length == 0
  puts "The line is currently empty."
  else
- katz_deli.each_with_index do |name, index|
-   puts "The line is currently: #{index +1}. #{name}"
+ x.each_with_index do |name, index|
+   line_push("#{index +1}. #{name}")
  end
+ puts "The line is currently: #{line_array.join(" ")}"
+end
 end
 
-katz_deli(name, index)
-other_deli(name, index)
+
+
+katz_deli = []
+
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
